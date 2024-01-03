28c3f99719a1890fbb6991ec1e15a219d82b1e74
German III <germaniiifelisarta@gmail.com>
2024-01-03 11:54:26 +0800

    Add Routes

--- frontend/root/src/main.jsx
+++ frontend/root/src/main.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./home/App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/register",
+    element: <h2>Register Page</h2>,
+  },
+  {
+    path: "/chat",
+    element: <h2>Chat Page</h2>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
