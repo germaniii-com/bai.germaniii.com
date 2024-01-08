74b513875a942d28d07345c0a959dd00a3adb433
German III <germaniiifelisarta@gmail.com>
2024-01-08 10:22:57 +0800

    Add styling to Navbar and Footer

--- frontend/root/src/components/Footer/Footer.jsx
+++ frontend/root/src/components/Footer/Footer.jsx
@@ -1,7 +1,60 @@
 import style from "./index.module.scss";
+import {
+  SiYoutube,
+  SiLinkedin,
+  SiInstagram,
+  SiGithub,
+  SiGmail,
+} from "react-icons/si";
+
+const socialLinks = [
+  {
+    icon: (
+      <SiGmail className="fill-primary w-3 md:group-hover:motion-safe:animate-bounce" />
+    ),
+    link: "mailto:germaniiifelisarta@gmail.com",
+  },
+  {
+    icon: (
+      <SiGithub className="fill-primary w-3 md:group-hover:motion-safe:animate-bounce" />
+    ),
+    link: "https://github.com/germaniii",
+  },
+  {
+    icon: (
+      <SiInstagram className="fill-primary w-3 md:group-hover:motion-safe:animate-bounce" />
+    ),
+    link: "https://instagram.com/germaniiifelisarta",
+  },
+  {
+    icon: (
+      <SiLinkedin className="fill-primary w-3 md:group-hover:motion-safe:animate-bounce" />
+    ),
+    link: "https://www.linkedin.com/in/german-iii-felisarta-648b9420b",
+  },
+  {
+    icon: (
+      <SiYoutube className="fill-primary w-3 md:group-hover:motion-safe:animate-bounce" />
+    ),
+    link: "https://www.youtube.com/channel/UCLdx8-LN-JAnrExazmhjmDA",
+  },
+];
 
 const Footer = () => {
-  return <footer className={style.footer}>Footer</footer>;
+  return (
+    <footer className={style.footer}>
+      <div className={style.title}>German III</div>
+      <ul>
+        {socialLinks.map((social) => (
+          <div key={social.link} className={style.item}>
+            <a key={social.link} href={social.link}>
+              <li key={social.link}>{social.icon}</li>
+            </a>
+          </div>
+        ))}
+      </ul>
+    </footer>
+  );
 };
 
 export default Footer;
--- frontend/root/src/components/Navbar/Navbar.jsx
+++ frontend/root/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,17 @@
 import style from "./index.module.scss";
+import { BiSun } from "react-icons/bi";
 
 const Navbar = () => {
-  return <nav className={style.navbar}>Navbar</nav>;
+  return (
+    <nav className={style.navbar}>
+      <span>
+        <div className={style.capitalTitle}>B</div>eyond{" "}
+        <div className={style.capitalTitle}>A</div>utomated{" "}
+        <div className={style.capitalTitle}>I</div>nteraction{" "}
+      </span>
+      <BiSun />
+    </nav>
+  );
 };
 
 export default Navbar;
