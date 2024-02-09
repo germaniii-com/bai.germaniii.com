114da3a043af5cfd2d81b029bc2b855c49e47f77
German III <germaniiifelisarta@gmail.com>
2024-02-09 10:02:30 +0800

    Add universalcookie

--- frontend/root/src/utils/axiosInstance.js
+++ frontend/root/src/utils/axiosInstance.js
@@ -1,6 +1,8 @@
 import axios from "axios";
+import Cookies from "universal-cookie";
 
 const init = (path = "") => {
+  const cookies = new Cookies();
   const instance = axios.create({
     baseURL: `bai-laravel.germaniii.com${path}`,
     withCredentials: true,
@@ -11,6 +13,7 @@ const init = (path = "") => {
   instance.interceptors.request.use((config) => {
     config.headers.Accept = "application/json";
     config.headers["Content-Type"] = "application/json";
+    config.headers["X-XSRF-TOKEN"] = cookies.get("XSRF-TOKEN");
     return config;
   });
 
